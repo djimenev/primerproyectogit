962d2546bd27702c5549b208387e636b7606937f
djimenev <diegojv10@hotmail.com>
2022-07-18 11:42:15 +0200

    nuevo commit

---
+++ proyecto1.js
@@ -0,0 +1,61 @@
+function crearVector(n, m){
+    let miarray = [];
+
+    for(let x = 0; x < n; x++){
+        miarray.push((Math.random() * m).toFixed(1));
+    }
+
+    return miarray
+}
+
+function sumaVector(v1,v2){
+    let miarray=[]
+    for(x=0;x<v1.length;x++){
+        
+        if(v1.length = v2.length){
+            miarray[x] = v1[x] + v2[x]
+        }
+    }
+    return miarray
+}
+console.log(sumaVector([1,2,3],[1,2,3]));
+
+
+function restaVector(v1,v2){
+    let miarray=[]
+    for(x=0;x<v1.length;x++){
+        
+        if(v1.length = v2.length){
+            miarray[x] = v1[x] - v2[x]
+        }
+    }
+    return miarray
+}
+
+console.log(restaVector([1,2,3],[1,2,3]));
+
+function productoVector(v1,v2){
+    let miarray = [];
+    for(x=0;x<v1.length;x++){
+        
+        if(v1.length = v2.length){
+            miarray[x] = v1[x] * v2[x]
+        }
+    }
+    return miarray
+}
+
+console.log(productoVector([1,2,3],[1,2,3]))
+
+function productoNumeroVector(v1,v2){
+    let miarray = [];
+    for(x=0;x<v1.length;x++){
+        
+        if(v1.length = v2.length){
+            miarray.push(n[x] * v);
+        }
+    }
+    return miarray
+}
+
+module.exports = {productoNumeroVector, sumaVector, restaVector,productoVector,crearVector}
